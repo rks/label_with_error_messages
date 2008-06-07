6e9ab884942a9eee9f1c8fdfc7d22d0bf3ccb605
Randy Souza <randy@frink.(none)>
2008-06-07 17:32:18 -0400

    Added label_with_error_messages

---
+++ lib/label_with_error_messages.rb
@@ -0,0 +1,13 @@
+module FancyViews
+	module ToSentence
+		def to_sentence; self.to_s; end
+	end
+
+	module LabelHelper
+		def label_with_error_messages
+			#
+		end
+  end
+end
+
+[NilClass, String].each { |klass| klass.class_eval { include FancyViews::ToSentence } }
---
+++ test/label_with_error_messages_test.rb
@@ -0,0 +1,20 @@
+require File.join(File.dirname(__FILE__), "test_helper")
+require File.join(File.dirname(__FILE__), "..", "lib", "label_with_error_messages")
+
+class ToSentenceTest < ActionView::TestCase
+	def test_monkeypatches_nil
+	  assert_nothing_raised do
+			assert_equal "", nil.to_sentence
+		end
+	end
+
+	def test_monkeypatches_string
+	  assert_nothing_raised do
+			assert_equal "", "".to_sentence
+		end
+	end
+end
+
+class LabelWithErrorMessagesTest < ActionView::TestCase
+
+end

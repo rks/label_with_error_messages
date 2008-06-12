fa748ca969bad0ea36763da62feb8676df516258
Randy Souza <randy@frink.(none)>
2008-06-12 00:22:11 -0400

    Re-implemented tests that Scribes so nicely destroyed...

--- lib/label_with_error_messages.rb
+++ lib/label_with_error_messages.rb
@@ -4,10 +4,21 @@ module FancyViews
   end
 
   module LabelHelper
-    def label_with_error_messages
-      #
+    def self.included(includer)
+      includer.alias_method_chain :label, :error_messages
+    end
+
+    def label_with_error_messages(object_name, method, text = nil, options = {})
+      model = instance_variable_get "@#{object_name}"
+      if model && model.errors.on(method)
+        text ||= method.to_s.humanize + " " + model.errors.on(method).to_sentence
+        options[:class] = (options[:class] || "") << " with_errors"
+        options[:class].strip!
+      end
+      label_without_error_messages(object_name, method, text, options)
     end
   end
 end
 
 [NilClass, String].each { |klass| klass.class_eval { include FancyViews::ToSentence } }
+ActionView::Helpers::FormHelper.send :include, FancyViews::LabelHelper
--- test/label_with_error_messages_test.rb
+++ test/label_with_error_messages_test.rb
@@ -15,6 +15,69 @@ class ToSentenceTest < ActionView::TestCase
   end
 end
 
-class LabelWithErrorMessagesTest < ActionView::TestCase
+module LabelAssertions
+  def assert_label_text(expected, actual)
+    assert_match(/<label([^>]*)>#{expected}<\/label>/, actual)
+  end
 
+  def assert_label_class(expected, actual)
+    assert_match(/<label([^>]*)class="#{expected}"([^>]*)>([^<]+<\/label>)/, actual)
+  end
+end
+
+class LabelWithErrorMessagesTest < ActionView::TestCase
+  include LabelAssertions
+
+  def setup
+    @errors = stub :on => nil
+    @person = stub :errors => @errors
+  end
+
+  def test_should_add_label_with_error_messages_to_chain
+    # An ActionView::TestCase acts as the erb execution environment
+    assert self.respond_to?(:label_without_error_messages)
+  end
+
+  def test_should_behave_normally_when_object_name_is_not_an_instance_variable
+    assert_dom_equal %(<label for="organization_name">Name</label>), label(:organization, :name)
+  end
+
+  def test_should_ask_an_instance_variable_if_it_has_errors_on_the_attribute
+    @errors.expects(:on).with(:name).returns(nil)
+    label :person, :name
+  end
+
+  def test_should_not_alter_label_text_if_attribute_is_valid
+    assert_label_text "Name", label(:person, :name)
+  end
+
+  def test_should_add_error_messages_to_text_if_attribute_is_not_valid
+    @errors.stubs(:on).with(:name).returns(["can't be blank"])
+    assert_label_text "Name can't be blank", label(:person, :name)
+  end
+
+  def test_should_not_add_error_messages_if_text_is_explicity_set
+    @errors.stubs(:on).with(:name).returns(["can't be blank"])
+    assert_label_text "Nombre", label(:person, :name, "Nombre")
+  end
+
+  def test_should_join_two_errors_with_and
+    @errors.stubs(:on).with(:name).returns(["has error one", "has error two"])
+    assert_label_text "Name has error one and has error two", label(:person, :name)
+  end
+
+  def test_should_join_three_errors_with_commas
+    @errors.stubs(:on).with(:name).returns(["has error one", "has error two", "has error three"])
+    assert_label_text "Name has error one, has error two, and has error three", label(:person, :name)
+  end
+
+  def test_should_set_label_class_to_with_errors_when_no_class_is_set
+    @errors.stubs(:on).with(:name).returns(["can't be blank"])
+    assert_label_class "with_errors", label(:person, :name)
+  end
+
+  def test_should_append_with_errors_as_label_class_when_class_is_set
+    @errors.stubs(:on).with(:name).returns(["can't be blank"])
+    assert_label_class "required with_errors", label(:person, :name, nil, :class => "required")
+  end
 end
